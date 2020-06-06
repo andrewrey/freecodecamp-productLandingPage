178025f4d9cef1070bf2b1528f2229f6a71d571b
Andrew Reynolds <andrew_reynolds@mac.com>
2020-06-05 17:49:42 -0700

    productcard function with products array

--- js/scripts.js
+++ js/scripts.js
@@ -5,48 +5,68 @@ const products = document.querySelector('.productsWrap');
 
 
 
-// functions
+// functions for producing product Card and Module Cards
 
-function productCards(item){
-    let module = document.createElement('div');
-    module.innerHTML = `
-        <h3>${item.name}</h3>
-        <figure class="modImage><img src=${itme.image} alt=${item.name}></figure>
-        <h4>Price</h4>
-        <p>${item.price}</p>
-        <h4>Decription</h4>
-        <p>${item.desc}</p>
+function productCards(item, index){
+    let HTML = `
+        <div class="product" data-index=${index}>
+            <h3>${item.name}</h3>
+            <figure class="modImage">
+                <img src=${item.image} alt=${item.name}>
+            </figure>
+        </div>
     `;
-    return module;
+    products.innerHTML += HTML;
 }
+
+
+
+
+// Overlay Module Template
+
+{/* 
+<div class="module">
+    <h3>Fish Type</h3>
+    <figure class="modImage">
+        <img src="img/catch-fish-fish-market-fishing-229789.jpg" alt="fish photo">
+    </figure>
+    <h4>Price:</h4>
+    <p>$3.14/lbs</p>
+    <h4>Description:</h4>
+    <p>Awesome fish, great for BBQ</p>
+</div> 
+
+*/}
 
 
 
 // product Object /// 
 
-const products = [
+const productsList = [
     {
         name: "big fish",
-        image: "",
+        image: "img/skeletonFish.jpg",
         price: "$3.00/pds", 
         desc: "Great when you're having guests over for dinner"
     },
     {
         name: "awesome fish",
-        image: "",
+        image: "img/fish1.jpg",
         price: "$5.50/pds", 
         desc: "For the price you get an amazing taste which you can enjoy"
     },
     {
         name: "expensive fish",
-        image: "",
+        image: "img/fish2.jpg",
         price: "$15.00/pds", 
         desc: "For those special nights"
     },
     {
         name: "wonderful fish",
-        image: "",
+        image: "img/fish3.jpg",
         price: "$12.99/pds", 
         desc: "When you feel like treating yourself to something great"
     }
 ]
+
+productsList.forEach((item,index)=>productCards(item,index));
